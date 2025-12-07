1fbe4726c36c556eece2831e158948a054d1bc51
FIBER-LABEL\fiber <fiberlabelace@gmail.com>
2025-12-07 08:03:46 +0700

    them 1 dong moi

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace LearningProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Learning Project");
         }
     }
 }
